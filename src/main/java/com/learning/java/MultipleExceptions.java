7d7035b29e5791c90d687918917f8a626481bd6b
Jacek Cislo <gecaj24@gmail.com>
2016-07-30 22:43:56 +0200

    Add class for multiple exceptions.

---
+++ src/main/java/com/learning/java/MultipleExceptions.java
@@ -0,0 +1,12 @@
+package com.learning.java;
+
+/**
+ * Created by jcislo on 30.07.16.
+ */
+
+/**
+ * http://docs.oracle.com/javase/7/docs/technotes/guides/language/catch-multiple.html
+ */
+public class MultipleExceptions {
+
+}
