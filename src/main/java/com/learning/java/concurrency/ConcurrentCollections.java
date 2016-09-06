05430bd3377a0bbe5e62d52dcbff2dfb41c03eba
Jacek Cislo <jacek.cislo@nokia.com>
2016-09-06 09:07:05 +0200

    Add concurrent collections example

---
+++ src/main/java/com/learning/java/concurrency/ConcurrentCollections.java
@@ -0,0 +1,30 @@
+package com.learning.java.concurrency;
+
+import java.util.Deque;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Created by cislo on 9/6/16.
+ */
+public class ConcurrentCollections {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new ConcurrentHashMap<>();
+        map.put("zebra", 52);
+        map.put("elephant", 10);
+        System.out.println(map.get("elephant"));
+        Queue<Integer> queue = new ConcurrentLinkedQueue<>();
+        queue.offer(31);
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        Deque<Integer> deque = new ConcurrentLinkedDeque<>();
+        deque.offer(10);
+        deque.push(4);
+        System.out.println(deque.peek());
+        System.out.println(deque.pop());
+
+    }
+}
