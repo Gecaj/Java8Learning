53a12c19b400354ad12a1d96bb24395bba023544
Jacek Cislo <jacek.cislo@nokia.com>
2016-11-08 11:33:25 +0100

    add OddOccurencesInArray solution

--- src/main/java/codility/OddOccurencesInArray.java
+++ src/main/java/codility/OddOccurencesInArray.java
@@ -5,4 +5,17 @@ package codility;
  */
 public class OddOccurencesInArray {
 
+    public int solution(int[] A) {
+        int unpaired = 0;
+        for (int i : A) {
+            unpaired ^= i;
+        }
+        return unpaired;
+    }
+
+    public static void main(String[] args) {
+        OddOccurencesInArray oddOccurencesInArray = new OddOccurencesInArray();
+        System.out.println(oddOccurencesInArray.solution(new int[]{1,2,3,4,3,2,1}));
+    }
+
 }
